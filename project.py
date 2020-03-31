1d5e5638abefc79d269981e67f8144d2105d4b1f
Chul Chong <chulchong1030@gmail.com>
2020-03-31 03:45:59 -0400

    added csv data files and testing pandas

--- project.py
+++ project.py
@@ -1,1 +1,4 @@
-print('hello World!')
+import pandas as pandas
+
+data_frame = pandas.read_csv('/Users/chulchong/Documents/workspace-sts-3.9.8.RELEASE/Python_project/test.csv')
+print(data_frame.head(2))
