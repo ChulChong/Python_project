fb38849f4a8f307085a36e4103baba73c6fec6b4
Chul Chong <chulchong1030@gmail.com>
2020-04-09 03:08:39 -0400

    still need to fix some var

--- project.py
+++ project.py
@@ -1,48 +1,28 @@
 from sklearn.model_selection import KFold
-from sklearn import metrics
 import numpy as numpy
 import pandas as pandas
-import matplotlib.pyplot as plt
 from sklearn import metrics, preprocessing
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
+pandas.options.mode.chained_assignment = None
 
 data_frame = pandas.read_csv("train.csv")
 test = pandas.read_csv("test.csv")
 loan_approval = data_frame['Loan_Status'].value_counts()['Y']
 
-# Function to output percentage row wise in a cross table
+# Basic set up for training.
 
-
-def percentageConvert(ser):
-    return ser/float(ser[-1])
-
-
-# Replace missing value of Self_Employed with more frequent category
-data_frame['Self_Employed'].fillna('No', inplace=True)
-
-# Add both ApplicantIncome and CoapplicantIncome to TotalIncome
 data_frame['TotalIncome'] = data_frame['ApplicantIncome'] + \
     data_frame['CoapplicantIncome']
-
-# Looking at the distribtion of TotalIncome
-data_frame['LoanAmount'].hist(bins=20)
-# Perform log transformation of TotalIncome to make it closer to normal
 data_frame['LoanAmount_log'] = numpy.log(data_frame['LoanAmount'])
 
-# Looking at the distribtion of TotalIncome_log
-data_frame['LoanAmount_log'].hist(bins=20)
-# Impute missing values for Gender
+# filling all missing data in train.csv
+
+data_frame['Self_Employed'].fillna('No', inplace=True)
 data_frame['Gender'].fillna(data_frame['Gender'].mode()[0], inplace=True)
-
-# Impute missing values for Married
 data_frame['Married'].fillna(data_frame['Married'].mode()[0], inplace=True)
-
-# Impute missing values for Dependents
 data_frame['Dependents'].fillna(
     data_frame['Dependents'].mode()[0], inplace=True)
-
-# Impute missing values for Credit_History
 data_frame['Credit_History'].fillna(
     data_frame['Credit_History'].mode()[0], inplace=True)
 
@@ -51,14 +31,8 @@ cat = ['Gender', 'Married', 'Dependents', 'Education',
        'Self_Employed', 'Credit_History', 'Property_Area']
 
 for var in cat:
-    le = preprocessing.LabelEncoder()
-    data_frame[var] = le.fit_transform(data_frame[var].astype('str'))
-data_frame.dtypes
-
-
-# Import models from scikit learn module:
-
-# Generic function for making a classification model and accessing performance:
+    LabelEncoder = preprocessing.LabelEncoder()
+    data_frame[var] = LabelEncoder.fit_transform(data_frame[var].astype('str'))
 
 
 def classification_model(model, data, predictors, outcome):
@@ -72,7 +46,7 @@ def classification_model(model, data, predictors, outcome):
     accuracy = metrics.accuracy_score(predictions, data[outcome])
     print("Accuracy : %s" % "{0:.3%}".format(accuracy))
 
-    # Perform k-fold cross-validation with 5 folds
+    # Perform k-fold cross-validation with 5 splits
     kf = KFold(n_splits=5)
     error = []
     for train, test in kf.split(data[predictors]):
@@ -104,6 +78,7 @@ fullData = pandas.concat([data_frame, test], axis=0)
 # Look at the available missing values in the dataset
 fullData.isnull().sum()
 # Identify categorical and continuous variables
+Order_col = ['Order_Number']
 ID_col = ['Loan_ID']
 target_col = ["Loan_Status"]
 cat_cols = ['Credit_History', 'Dependents', 'Gender',
@@ -134,8 +109,7 @@ fullData['TotalIncome'] = fullData['ApplicantIncome'] + \
 
 fullData['TotalIncome_log'] = numpy.log(fullData['TotalIncome'])
 
-# Histogram for Total Income
-fullData['TotalIncome_log'].hist(bins=20)
+
 # create label encoders for categorical features
 for var in cat_cols:
     number = LabelEncoder()
@@ -151,6 +125,7 @@ predictors_Logistic = ['Credit_History', 'Education', 'Gender']
 
 x_train = train_modified[list(predictors_Logistic)].values
 y_train = train_modified["Loan_Status"].values
+
 
 x_test = test_modified[list(predictors_Logistic)].values
 # Create logistic regression object
@@ -172,5 +147,5 @@ outcome_var = 'Loan_Status'
 
 classification_model(model, data_frame, predictors_Logistic, outcome_var)
 
-test_modified.to_csv("Logistic_Prediction.csv",
+test_modified.to_csv("Loan_Application_Approval_Tested.csv",
                      columns=['Loan_ID', 'Loan_Status'])
